31ec7bfd74d4934417ef0a7eac8e1ee13b048330
Flavien Pictet <flavien@pictet.net>
2023-07-11 14:14:47 +0200

    commiting

--- currencies.py
+++ currencies.py
@@ -1,11 +1,17 @@
-# pylint: disable=missing-docstring
+""" blabla """
 
-# TODO: add some currency rates
-RATES = {}
+
+RATES = {
+    "USDEUR" : 0.85,
+    "GBPEUR" : 1.13,
+    "CHFEUR" : 0.86,
+    "EURGBP" : 0.885
+}
+
 
 def convert(amount, currency):
-    """returns the converted amount in the given currency
-    amount is a tuple like (100, "EUR")
-    currency is a string
-    """
-    pass  # YOUR CODE HERE
+    """ blabla """
+
+    if RATES.get( amount[1] + currency , "toto") is not "toto":
+        amount_ = round(amount[0] * RATES.get( amount[1] + currency))
+        return amount_
